07d30d3f1f07726752e75071201fbb0b09e48612
rodolfo.gsilva2 <rodolfo.gsilva2@senacsp.edu.br>
2022-08-31 20:37:49 -0300

    Criação da classe calculadora

---
+++ circ02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace circ02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
