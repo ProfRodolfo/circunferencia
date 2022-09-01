88acc31576b12baad445939456bb68212eed36e3
rodolfo.gsilva2 <rodolfo.gsilva2@senacsp.edu.br>
2022-08-31 21:28:04 -0300

    Chamadas do método principal

--- circ02/Program.cs
+++ circ02/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-
+using System.Globalization;
 namespace circ02
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculadora calc = new Calculadora();
+
+            Console.Write("Entre com o valor do raio");
+            double raio = double.Parse(Console.ReadLine(),
+                CultureInfo.InvariantCulture);
+
+            double cir = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
+
+            Console.WriteLine("Circunferência: " +
+                cir.ToString("F2", 
+                CultureInfo.InvariantCulture));  
+            Console.WriteLine("Volume: " +
+                volume.ToString("F2", 
+                CultureInfo.InvariantCulture)); 
+            Console.WriteLine("Volume: " +
+                calc.Pi.ToString("F2",
+                CultureInfo.InvariantCulture));
         }
     }
 }
